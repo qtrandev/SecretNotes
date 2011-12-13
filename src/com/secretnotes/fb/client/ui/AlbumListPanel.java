ec783ee29103b60816a5b9fecd63eda8a6c3b566
Quyen Tran <qtrandev@gmail.com>
2011-12-12 21:14:23 -0500

    Clean up AlbumListPanel layout

--- src/com/secretnotes/fb/client/ui/AlbumListPanel.java
+++ src/com/secretnotes/fb/client/ui/AlbumListPanel.java
@@ -2,7 +2,6 @@ package com.secretnotes.fb.client.ui;
 
 import java.util.ArrayList;
 
-import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.gwt.user.client.ui.FlowPanel;
 import com.google.gwt.user.client.ui.Label;
@@ -16,27 +15,15 @@ public class AlbumListPanel extends FlowPanel {
 
 	public AlbumListPanel() {
 		super();
-		titleLabel = new Label("Albums");
-		titleLabel.setStyleName("albumTitle");
-		add(titleLabel);
-		albumPhotoPanels = new ArrayList<AlbumPhotoPanel>();
+		init();
+		resetPanel();
 	}
 	
-//	public void displayPhotos(ArrayList<Photo> photos) {
-//		resetPanel();
-//		add(getTitleLabel());
-//		AlbumPhotoPanel albumPhotoPanel;
-//		for (Photo photo : photos) {
-//			albumPhotoPanel = new AlbumPhotoPanel();
-//			albumPhotoPanel.setClickHandler(new ClickHandler() {
-//				public void onClick(ClickEvent event) {
-//					
-//				}
-//			});
-//			albumPhotoPanel.displayPhoto(photo);
-//			add(albumPhotoPanel);
-//		}
-//	}
+	private void init() {
+		titleLabel = new Label("Albums");
+		titleLabel.setStyleName("albumTitle");
+		getAlbumPhotoPanels();
+	}
 	
 	public void addAlbum(Album album, ClickHandler clickHandler) {
 		AlbumPhotoPanel albumPhotoPanel = new AlbumPhotoPanel(album);
@@ -56,8 +43,10 @@ public class AlbumListPanel extends FlowPanel {
 		}
 	}
 	
-	private void resetPanel() {
+	public void resetPanel() {
 		clear();
+		add(getTitleLabel());
+		getAlbumPhotoPanels().clear();
 	}
 	
 	private Label getTitleLabel() {
@@ -65,6 +54,9 @@ public class AlbumListPanel extends FlowPanel {
 	}
 
 	public ArrayList<AlbumPhotoPanel> getAlbumPhotoPanels() {
+		if (albumPhotoPanels == null) {
+			albumPhotoPanels = new ArrayList<AlbumPhotoPanel>();
+		}
 		return albumPhotoPanels;
 	}
 	
